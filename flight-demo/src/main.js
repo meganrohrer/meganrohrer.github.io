6d2a5c02aeb8a5bcb0bd564658a2a5b8e97187d0
Megan Rohrer <92053999+meganrohrer@users.noreply.github.com>
2026-03-19 14:48:38 -0400

    Create main.js

---
+++ flight-demo/src/main.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-layer-list";
+import "@arcgis/map-components/components/arcgis-legend";
+import "@arcgis/map-components/components/arcgis-table-list";
+import "@arcgis/map-components/components/arcgis-expand";
+
+import "@esri/calcite-components/components/calcite-shell";
+import "@esri/calcite-components/components/calcite-panel";
+import "@esri/calcite-components/components/calcite-navigation";
+
+view.ui.add("toggle-snippet", "top-right");
